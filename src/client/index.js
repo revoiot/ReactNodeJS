307f446672e019709f45a885e73404070be1f88e
Shimpei horikawa <shimpei@ShimpeinoMacBook-Pro.local>
2020-06-20 23:08:58 +0900

    initial commit

---
+++ src/client/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const Index = () => {
+//   return <div>Hello React!</div>;
+// };
+
+// Spaceを開けてlintエラーを起こす
+fetch(　'/api/'　).then(response => {
+  console.log(response.json());
+})
+export const Index = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('index')); 
