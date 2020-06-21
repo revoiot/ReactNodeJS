78d63063c31a93fad5baddeb02205208cda1c53e
Shimpei horikawa <shimpei@ShimpeinoMacBook-Pro.local>
2020-06-21 17:30:43 +0900

    master commit

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import path from 'path';
+import config from 'config';
 
 const app = express();
+const serverConfig = config.get('server');
 
 app.use(express.static(path.join('./', 'dist')));
 
@@ -15,4 +17,8 @@ app.get('*', function (req, res) {
 
 app.listen(3000, ()=> {
   console.log('server running');
+})
+
+app.listen(serverConfig.port, ()=> {
+  console.log(`server starting -> [port] ${serverConfig.port} [env] ${process.env.NODE_ENV}`);
 })
