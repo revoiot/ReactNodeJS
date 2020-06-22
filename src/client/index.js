8d29a4afa8fa47ac39b1bbed9ef9036c8d0488e0
revoiot <revowater@gmail.com>
2020-06-22 15:48:06 +0900

    YES I DO

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // const Index = () => {
 //   return <div>Hello React!</div>;
 // };
 
 // Spaceを開けてlintエラーを起こす
-fetch('/api/').then(response => {
+fetch("/api/").then((response) => {
   console.log(response.json());
-})
+});
 
 export const Index = () => {
   return <div>Hello React!</div>;
 };
 
-ReactDOM.render(<Index />, document.getElementById('index')); 
+ReactDOM.render(<Index />, document.getElementById("index"));
