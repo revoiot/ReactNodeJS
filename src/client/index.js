8d3a993d532e23582cd7990c0d01eb37c5920abc
revoiot <revowater@gmail.com>
2020-06-22 22:33:43 +0900

    vf

--- src/client/index.js
+++ src/client/index.js
@@ -6,9 +6,9 @@ import ReactDOM from 'react-dom';
 // };
 
 // Spaceを開けてlintエラーを起こす
-fetch(  '/api/'   ).then((response) => {
+fetch(  '/api/'   ).then(response => {
   console.log(response.json());
-});
+})
 
 export const Index = () => {
   return <div>Hello React!</div>;
